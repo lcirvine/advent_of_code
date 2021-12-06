60eeb4463a887fbc5cb15965dec1608b9855dff0
lcirvine <55790560+lcirvine@users.noreply.github.com>
2021-12-06 14:18:44 +0000

    adding day 4

--- day4.py
+++ day4.py
@@ -24,14 +24,37 @@ def read_input(file: str = 'day4.txt'):
 
 
 def part_1():
-    nums, boards = read_input('day4_test.txt')
+    nums, boards = read_input('day4.txt')
     for num in nums:
+        # marking the boards by setting number called to nan
         for n, board in boards.items():
             board.replace(num, np.nan, inplace=True)
+            # checking the boards for a winner
+            for col in board.columns:
+                if board[col].isna().all():
+                    return int(board.sum().sum() * num)
+            for i, row in board.iterrows():
+                if row.isna().all():
+                    return int(board.sum().sum() * num)
 
 
 def part_2():
-    pass
+    nums, boards = read_input('day4.txt')
+    possible_boards = list(boards.keys())
+    for num in nums:
+        for n, board in boards.items():
+            # marking the boards by setting number called to nan
+            board.replace(num, np.nan, inplace=True)
+            # checking the boards for a winner
+            for col in board.columns:
+                if board[col].isna().all() and n in possible_boards:
+                    possible_boards.remove(n)
+            for i, row in board.iterrows():
+                if row.isna().all() and n in possible_boards:
+                    possible_boards.remove(n)
+            # checking to see how many boards are left
+            if len(possible_boards) == 0:
+                return int(board.sum().sum() * num)
 
 
 if __name__ == '__main__':
