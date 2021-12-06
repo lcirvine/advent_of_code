cf90d103ed399ea5ec1e3400abb06477776a638f
lcirvine <lance.c.irvine@gmail.com>
2021-12-06 14:24:46 +0000

    starting day 5

---
+++ day5.py
@@ -0,0 +1,20 @@
+import os
+
+
+def read_input(file: str = 'day_5.txt'):
+    with open(os.path.join('inputs', file)) as f:
+        data = f.read().split('\n')
+    return data
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
