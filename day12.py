a02eac6727b112dbaa9ff9e9f58213d0fcf8c42c
lcirvine <lance.c.irvine@gmail.com>
2021-12-14 10:11:27 +0000

    setting up day 14

--- day12.py
+++ day12.py
@@ -7,23 +7,28 @@ def read_input(file: str):
     with open(os.path.join('inputs', file)) as f:
         for i in f.read().split('\n'):
             c1, c2 = i.split('-')
-            if c2 != 'start':
-                caves[c1].append(c2)
-            if c1 != 'start':
-                caves[c2].append(c1)
+            caves[c1].append(c2)
+            caves[c2].append(c1)
     return caves
 
 
-def part_1():
-    caves = read_input('day12_test.txt')
-    all_paths = []
+caves = read_input('day12.txt')
 
 
-
-def part_2():
-    pass
+def cave_count(part, seen=[], cave='start'):
+    if cave == 'end':
+        return 1
+    if cave in seen:
+        if cave == 'start':
+            return 0
+        if cave.islower():
+            if part == 1:
+                return 0
+            else:
+                part = 1
+    return sum(cave_count(part, seen+[cave], n) for n in caves[cave])
 
 
 if __name__ == '__main__':
-    print(part_1())
-    print(part_2())
+    print(cave_count(part=1))
+    print(cave_count(part=2))
