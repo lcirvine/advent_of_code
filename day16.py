f37bb057a80f1a87188c92cc2bae6d9085ae59b2
lcirvine <lance.c.irvine@gmail.com>
2021-12-16 23:34:21 +0000

    day 16 pt 1

--- day16.py
+++ day16.py
@@ -10,10 +10,9 @@ with open(os.path.join('inputs', 'day16_key.txt')) as f:
     # hexkey = {i.split(' = ')[0]: i.split(' = ')[1] for i in f.read().split('\n')}
 
 
-
-def read_input(file: str):
+def read_input(file: str = 'day16.txt'):
     with open(os.path.join('inputs', file)) as f:
-        data = f.read().split('\n')
+        data = f.read()
     return data
 
 
@@ -21,31 +20,37 @@ def convert_to_binary(hexstr: str):
     return ''.join([hexkey[char] for char in hexstr])
 
 
-def parse_binary(binstr: str, sum_v=0, literal_val_total=0, remaining_packet_num=0, remaining_packet_len=0):
-
+def parse_binary(binstr: str, sum_v=0, val=0):
     sum_v += int(binkey['0' + binstr[:3]])
     t = int(binkey['0' + binstr[3:6]])
 
     if t == 4:  # literal value
-        packet_bits = binstr[6:]
-        first_bits = [p for p in packet_bits[::5]]
+        # remaining_packet_num = max([(remaining_packet_num - 1), 0])
+        binstr = binstr[6:]
+        first_bits = [p for p in binstr[::5]]
         # including the first packet that starts with 0
         first_zero = first_bits.index('0') + 1
         literal_val = ''
-        for packet in [packet_bits[x: x + 5] for x in range(0, 5 * first_zero, 5)]:
+        for packet in [binstr[x: x + 5] for x in range(0, 5 * first_zero, 5)]:
             literal_val += packet[1:5]
-        literal_val_total += int(literal_val, 2)
+        val += int(literal_val, 2)
+        # what's remaining at the end of this literal string?
+        binstr = binstr[5 * first_zero:]
+        # remaining_packet_len = max([(remaining_packet_num - (5 * first_zero) + 6), 0])
     else:  # operator
+        # remaining_packet_num = max([(remaining_packet_num - 1), 0])
         len_id = int(binstr[6:7])
         binstr = binstr[7:]
         if len_id == 0:
             sub_len = int(binstr[:15], 2)
             binstr = binstr[15:]
-            remaining_packet_len = sub_len
+            # remaining_packet_len += sub_len
         elif len_id == 1:
             sub_num = int(binstr[: 11], 2)
             binstr = binstr[11:]
-            remaining_packet_num = sub_num
+            # remaining_packet_num += sub_num
+    if len(binstr) > 8:
+        return parse_binary(binstr, sum_v, val)
     return sum_v
 
 
@@ -54,5 +59,6 @@ def part_2():
 
 
 if __name__ == '__main__':
-    binstr = convert_to_binary('D2FE28')
+    hexcode = read_input()
+    binstr = convert_to_binary(hexcode)
     print(parse_binary(binstr))
