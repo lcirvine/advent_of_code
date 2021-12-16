f164032f2ac4782d2ebb79253b30019c1bcf1dd9
lcirvine <55790560+lcirvine@users.noreply.github.com>
2021-12-16 22:27:24 +0000

    working on day 16

--- day15.py
+++ day15.py
@@ -2,13 +2,44 @@ import os
 
 
 def read_input(file: str):
+    cave = {}
     with open(os.path.join('inputs', file)) as f:
-        data = f.read().split('\n')
-    return data
+        for x, row in enumerate(f.read().split('\n')):
+            for y, risk in enumerate(row):
+                cave[(x, y)] = int(risk)
+    return cave
+
+
+
 
 
 def part_1():
-    pass
+    cave = read_input('day15_test.txt')
+    start = [c for c in cave][0]
+    destination = [c for c in cave][-1]
+    unvisited = {}
+
+    def neighbors(coord):
+        x, y = coord
+        # return filter(lambda n: n in cave, [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)])
+        return filter(lambda n: n in cave, [(x, y + 1), (x + 1, y)])
+
+    all_risks = []
+
+
+    def cave_risk(loc, path: list = [], risk=0):
+        if loc == destination:
+            all_risks.append(risk)
+        else:
+            risk += cave[loc]
+            path.append(loc)
+            for n in neighbors(loc):
+                return cave_risk(n, path, risk)
+
+    cave_risk(loc=start, risk=(0 - cave[start]))
+
+    return min(all_risks)
+
 
 
 def part_2():
--- day16.py
+++ day16.py
@@ -1,5 +1,15 @@
 import os
 
+with open(os.path.join('inputs', 'day16_key.txt')) as f:
+    hexkey = {}
+    binkey = {}
+    for i in f.read().split('\n'):
+        h, b = i.split(' = ')
+        hexkey[h] = b
+        binkey[b] = h
+    # hexkey = {i.split(' = ')[0]: i.split(' = ')[1] for i in f.read().split('\n')}
+
+
 
 def read_input(file: str):
     with open(os.path.join('inputs', file)) as f:
@@ -7,8 +17,36 @@ def read_input(file: str):
     return data
 
 
-def part_1():
-    pass
+def convert_to_binary(hexstr: str):
+    return ''.join([hexkey[char] for char in hexstr])
+
+
+def parse_binary(binstr: str, sum_v=0, literal_val_total=0, remaining_packet_num=0, remaining_packet_len=0):
+
+    sum_v += int(binkey['0' + binstr[:3]])
+    t = int(binkey['0' + binstr[3:6]])
+
+    if t == 4:  # literal value
+        packet_bits = binstr[6:]
+        first_bits = [p for p in packet_bits[::5]]
+        # including the first packet that starts with 0
+        first_zero = first_bits.index('0') + 1
+        literal_val = ''
+        for packet in [packet_bits[x: x + 5] for x in range(0, 5 * first_zero, 5)]:
+            literal_val += packet[1:5]
+        literal_val_total += int(literal_val, 2)
+    else:  # operator
+        len_id = int(binstr[6:7])
+        binstr = binstr[7:]
+        if len_id == 0:
+            sub_len = int(binstr[:15], 2)
+            binstr = binstr[15:]
+            remaining_packet_len = sub_len
+        elif len_id == 1:
+            sub_num = int(binstr[: 11], 2)
+            binstr = binstr[11:]
+            remaining_packet_num = sub_num
+    return sum_v
 
 
 def part_2():
@@ -16,5 +54,5 @@ def part_2():
 
 
 if __name__ == '__main__':
-    print(part_1())
-    print(part_2())
+    binstr = convert_to_binary('D2FE28')
+    print(parse_binary(binstr))
