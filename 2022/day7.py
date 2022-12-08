8b0130907ea026b00cfb89d8c2a778b345542749
Lance C. Irvine <lance.irvine@factset.com>
2022-12-08 10:46:59 +0000

    day 8 solutions, still working on day 7

---
+++ 2022/day7.py
@@ -0,0 +1,139 @@
+from aoc_inputs import get_input
+import re
+
+# commands = get_input()
+commands = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e   
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
+
+
+def dir_contents(dir_name: str, command_str: str = commands) -> list:
+    cd_dir_command = f"$ cd {dir_name}\n$ ls\n"
+    start_point = command_str.find(cd_dir_command) + len(cd_dir_command)
+    end_point = command_str.find('\n$', start_point)
+    return command_str[start_point: end_point].split('\n')
+
+
+def return_dir_details():
+    dir_pat = r"\$\scd\s([a-zA-Z/]*)"
+    all_dirs = [dr for dr in re.findall(dir_pat, commands) if dr != '']
+    dir_details = {}
+    for dr in all_dirs:
+        contents = dir_contents(dr)
+        files = [item for item in contents if 'dir ' not in item]
+        file_size = sum([int(re.match(r"(\d*)", f).group()) for f in files])
+        sub_dirs = [item.split('dir ')[1] for item in dir_contents(dr) if 'dir ' in item]
+        dir_details[dr] = {'contents': contents, 'files': files, 'file_size': file_size, 'sub_dirs': sub_dirs}
+    return dir_details
+
+
+def dirs_to_remove(dir_details: dict, limit=100000):
+    dir_list = []
+    for dr, details in dir_details.items():
+        if details['file_size'] > limit:
+            dir_list.append(dr)
+    return dir_list
+
+
+def sub_dirs_to_remove(dir_details: dict, dir_list: list):
+    if dir_list is None:
+        dir_list = []
+    for top_dir, details in dir_details.items():
+        for dr in dir_list:
+            if dr in details['sub_dirs']:
+                dir_list.append(top_dir)
+    return list(set(dir_list))
+
+
+def part_1_test():
+    dir_details = return_dir_details()
+    remove_list = dirs_to_remove(dir_details)
+    remove_list = sub_dirs_to_remove(dir_details, remove_list)
+    for dr in remove_list:
+        dir_details.pop(dr)
+    total = 0
+    for dir, details in dir_details.items():
+        total += details['file_size']
+
+
+def item_size(item: str):
+    size = 0
+    size_pat = r"(\d*)"
+    size_match = re.match(size_pat, item)
+    if size_match and size_match.group().isnumeric():
+        size += int(size_match.group())
+    return size
+
+
+def sub_dir_list(current_dir: str, dir_list=None):
+    if dir_list is None:
+        dir_list = []
+    sub_dirs = [item.split('dir ')[1] for item in dir_contents(current_dir) if 'dir ' in item]
+    if len(sub_dirs) > 0:
+        for sd in sub_dirs:
+            dir_list.append(sd)
+            sub_dir_list(current_dir=sd, dir_list=dir_list)
+    return dir_list
+
+
+def sub_dir_size(current_dir: str, total_dir_size=0, limit=100000):
+    total_dir_size += dir_size(current_dir)
+    sub_dirs = [item.split('dir ')[1] for item in dir_contents(current_dir) if 'dir ' in item]
+    if len(sub_dirs) > 0:
+        for sd in sub_dirs:
+            total_dir_size += dir_size(current_dir)
+            if total_dir_size > limit:
+                return None
+            else:
+                sub_dir_size(current_dir=sd, total_dir_size=total_dir_size)
+    return total_dir_size
+
+
+def dir_size(dir_name: str):
+    contents = dir_contents(dir_name)
+    files = [item for item in contents if 'dir ' not in item]
+    return sum([int(re.match(r"(\d*)", f).group()) for f in files])
+
+
+def part_1():
+    dir_pat = r"\$\scd\s([a-zA-Z/]*)"
+    dir_details = return_dir_details()
+    remove_list = dirs_to_remove(dir_details)
+    remove_list = sub_dirs_to_remove(dir_details, remove_list)
+    for dr in remove_list:
+        dir_details.pop(dr)
+    total_size = 0
+    for dr, details in dir_details.items():
+        total_size += details['file_size']
+        for sd in details['sub_dirs']:
+            total_size += dir_details.get(sd).get('file_size')
+    return total_size
+
+
+def part_2():
+    pass
+
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
---
+++ 2022/day8.py
@@ -0,0 +1,79 @@
+from aoc_inputs import get_input
+
+data = get_input().split('\n')
+
+tree_map = {}
+for y in range(len(data)):
+    for x in range(len(data[y])):
+        tree_map[(x, y)] = int(data[y][x])
+
+
+def part_1():
+    visible_trees = 0
+    min_x, min_y = min(tree_map)  # will be (0, 0)
+    max_x, max_y = max(tree_map)
+    for coord, height in tree_map.items():
+        x = coord[0]
+        y = coord[1]
+        # borders
+        if x == min_x or x == max_x:
+            visible_trees += 1
+        elif y == min_y or y == max_y:
+            visible_trees += 1
+        else:
+            # make sure to add +1 to get end of range and not to count the current tree!
+            left_trees = [tree_map[(xr, y)] for xr in range(min_x, x)]
+            right_trees = [tree_map[(xr, y)] for xr in range(x + 1, max_x + 1)]
+            up_trees = [tree_map[(x, yr)] for yr in range(min_y, y)]
+            down_trees = [tree_map[(x, yr)] for yr in range(y + 1, max_y + 1)]
+            if height > max(up_trees) or height > max(down_trees) or height > max(left_trees) or height > max(right_trees):
+                visible_trees += 1
+    return visible_trees
+
+
+def part_2():
+    min_x, min_y = min(tree_map)  # will be (0, 0)
+    max_x, max_y = max(tree_map)
+    scenic_scores = {}
+    for coord, height in tree_map.items():
+        x = coord[0]
+        y = coord[1]
+        left_trees_visible = 0
+        # you have to count out from the tree, not in toward the tree!
+        for lt in range(x - 1, min_x - 1, -1):
+            if tree_map[(lt, y)] >= height:
+                left_trees_visible += 1
+                break
+            else:
+                left_trees_visible += 1
+        right_trees_visible = 0
+        for rt in range(x + 1, max_x + 1):
+            if tree_map[(rt, y)] >= height:
+                right_trees_visible += 1
+                break
+            else:
+                right_trees_visible += 1
+        up_trees_visible = 0
+        # you have to count out from the tree, not in toward the tree!
+        for ut in range(y - 1, min_y - 1, -1):
+            if tree_map[(x, ut)] >= height:
+                up_trees_visible += 1
+                break
+            else:
+                up_trees_visible += 1
+        down_trees_visible = 0
+        for dt in range(y + 1, max_y + 1):
+            if tree_map[(x, dt)] >= height:
+                down_trees_visible += 1
+                break
+            else:
+                down_trees_visible += 1
+        scenic_scores[(x, y)] = (left_trees_visible * right_trees_visible * up_trees_visible * down_trees_visible)
+    max_score = max(scenic_scores.values())
+    # wouldn't you want to know what tree it was rather than the score?
+    return {tree: score for tree, score in scenic_scores.items() if score == max_score}
+
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
