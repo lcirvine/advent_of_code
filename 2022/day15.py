1988f17bcf9107027d1a7aff4dd871e0860ea5e1
lcirvine <lance.c.irvine@gmail.com>
2022-12-16 09:20:28 +0000

    day15 pt 2 still a work in progress

--- 2022/day15.py
+++ 2022/day15.py
@@ -10,29 +10,59 @@ def get_signal_map(test=False):
         sy, by = [int(y) for y in re.findall(r"y=([\-\d]*)", sensor)]
         dist = abs(sx - bx) + abs(sy - by)
         signal_map[(sx, sy)] = {'beacon': (bx, by), 'distance': dist}
-    return signal_map
+    return dict(sorted(signal_map.items()))
 
 
-def signal_area(signal: tuple, beacon: tuple, dist: int):
+def signal_area(signal: tuple, dist: int):
     """
     Don't do this. Your computer will freeze.
     """
-    sx, bx = signal
-    sy, by = beacon
+    sx, sy = signal
     signal_area = []
     for distx in range(dist + 1):
         for disty in range(dist - distx + 1):
-            sa_locs = [
+            signal_area.extend([
                 (sx - distx, sy - disty),
                 (sx + distx, sy - disty),
                 (sx - distx, sy + disty),
                 (sx + distx, sy + disty)
-            ]
-            signal_area.extend(list(filter(lambda n: n != (bx, by), sa_locs)))
+            ])
     return list(set(signal_area))
 
 
-def part_1(test=False):
+def signal_borders(signal: tuple, dist: int, max_val: int):
+    sx, sy = signal
+    # want to be outside the border
+    # dist += 1
+    borders = []
+    for dx in range(dist + 1):
+        dy = dist - dx
+        borders.extend([
+            (sx - dx, sy - dy),
+            (sx + dx, sy - dy),
+            (sx - dx, sy + dy),
+            (sx + dx, sy + dy)
+        ])
+    borders = list(set([b for b in borders if b[0] in range(0, max_val) and b[1] in range(0, max_val)]))
+    return borders
+
+
+def outside_signal_area(loc: tuple, signal_map: dict = None):
+    outside = []
+    x, y = loc
+    for sig, sigattrs in signal_map.items():
+        sx, sy = sig
+        dist = sigattrs['distance']
+        # is this even in range of the signal?
+        if (abs(x - sx) + abs(x - sy)) > dist:
+            outside.append(True)
+        else:
+            outside.append(False)
+            break
+    return all(outside)
+
+
+def part_1(test=True):
     """
     Triangles!!!
     """
@@ -57,8 +87,17 @@ def part_1(test=False):
     return len(unavail)
 
 
-def part_2():
-    pass
+def part_2(test=True):
+    signal_map = get_signal_map(test=test)
+    if test:
+        max_val = 20
+    else:
+        max_val = 4000000
+    for sig, sigattrs in signal_map.items():
+        borders = signal_borders(sig, sigattrs['distance'], max_val)
+        for loc in borders:
+            if outside_signal_area(loc, signal_map):
+                print(loc)
 
 
 if __name__ == '__main__':
