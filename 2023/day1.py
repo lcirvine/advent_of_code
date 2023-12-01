8994636cca1b90ce77b3ae21583333e36967013e
Lance C. Irvine <lance.irvine@factset.com>
2023-12-01 15:08:53 +0000

    day 1 complete

---
+++ 2023/day1.py
@@ -0,0 +1,38 @@
+import re
+import sys
+sys.path.append('..')
+from aoc_inputs import get_input
+
+
+def part_1(calibrations: list):
+    cal_val = 0
+    for text_line in calibrations:
+        vals = re.findall(r"\d", text_line)
+        if vals:
+            cal_val += int(vals[0] + vals[-1])
+    return cal_val
+
+
+def part_2(calibrations: list):
+    cal_val = 0
+    pat = r"(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))"
+    val_map = {
+        'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+    }
+    for text_line in calibrations:
+        vals = [num for group in re.findall(pat, text_line) for num in group if num]
+        if vals:
+            first_val = vals[0]
+            last_val = vals[-1]
+            if first_val in val_map:
+                first_val = val_map[first_val]
+            if last_val in val_map:
+                last_val = val_map[last_val]
+            cal_val += int(first_val + last_val)
+    return cal_val
+
+
+if __name__ == '__main__':
+    puzzle_data = get_input().split('\n')
+    print(part_1(puzzle_data))
+    print(part_2(puzzle_data))
---
+++ 2023/notes.md
@@ -0,0 +1,26 @@
+# Advent of Code 2023 Notes
+
+## Day 1
+### Part 1
+
+- Used regex to find all digits in string
+- Added the strings of digits ('1' + '2' = '12' rather than 1 + 2 = 3), then converted to an integer 
+
+### Part 2
+
+- Used regex to find either digits or words for numbers (i.e. 'one', 'two', etc.) 
+    ```python 
+        pat = r"(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))"
+    ```
+- Regex pattern required positive lookahead ```?=``` to find overlapping matches (i.e. 'oneight')
+- ```re.findall``` returned a list of tuples for each group, flattened with nested list comprehension
+  ```python
+  vals = [num for group in re.findall(pat, text_line) for num in group if num]
+  ```
+- Mapped word for number to digit (as a string) ```{'one': '1', 'two': '2'...}```
+
+#### Tags
+- regex
+- regex with multiple groups 
+- regex with positive lookahead
+- nested list comprehension
