f93f40d7c752fdcd1873931cdbe819f31ffd5b36
lcirvine <lance.c.irvine@gmail.com>
2024-12-10 00:31:03 +0000

    day 5 solutions

--- aoc_2024/day05.py
+++ aoc_2024/day05.py
@@ -4,7 +4,8 @@ import sys
 sys.path.append('..')
 from aoc_utils import get_input, submit_answer
 
-day_num = date.today().day
+
+day_num = 5
 data = get_input(day_num)
 rules_text, pages_text = data.split('\n\n')
 
@@ -18,44 +19,43 @@ print_instructions = []
 for instruction in pages_text.split('\n'):
     print_instructions.append([int(x) for x in instruction.split(',')])
 
+def passes_test(page_list):
+    for i, p in enumerate(page_list):
+        pages_before = page_list[:i]
+        page_rules = rules[p]
+        if any([x for x in pages_before if x in page_rules]):
+            return False
+    return True
+
+def get_mid_page(page_list):
+    ix_mid = len(page_list) // 2
+    return page_list[ix_mid]
+
 
 def part_1():
     total = 0
     for page_list in print_instructions: 
-        passes_test = True
-        for i, p in enumerate(page_list):
-            pages_before = page_list[:i]
-            page_rules = rules[p]
-            if any([x for x in pages_before if x in page_rules]):
-                passes_test = False
-                break
-        if passes_test:
-            ix_mid = len(page_list) // 2
-            page_mid = page_list[ix_mid]
-            total += page_mid
+        if passes_test(page_list):
+            total += get_mid_page(page_list)
     return total
 
 
 def part_2():
     total = 0
-    new_instructions = []
     for page_list in print_instructions: 
-        passes_test = True
-        for i, p in enumerate(page_list):
-            pages_before = page_list[:i]
-            page_rules = rules[p]
-            if any([x for x in pages_before if x in page_rules]):
-                passes_test = False
-                break
-        if not passes_test:
-            new_instructions.append(page_list)
-            ix_mid = len(page_list) // 2
-            page_mid = page_list[ix_mid]
-            total += page_mid
+        if not passes_test(page_list):
+            for i, p in enumerate(page_list):
+                    pages_before = page_list[:i]
+                    page_rules = rules[p]
+                    for _ in [x for x in pages_before if x in page_rules]:
+                            page_list.remove(p)
+                            ix = min([page_list.index(i) for i in rules[p] if i in page_list])
+                            page_list.insert(ix, p)
+            total += get_mid_page(page_list)
     return total
 
 
 if __name__ == '__main__':
     a1 = part_1()
     a2 = part_2()
-    submit_answer(answer_1=a1, answer_2=a2, day_num=day_num, submit=True)
+    submit_answer(answer_1=a1, answer_2=a2, day_num=day_num)
