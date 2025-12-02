28e04d2eda26749081809d6a6ba2db97192ff33c
Lance Irvine <lance.irvine@factset.com>
2025-12-02 19:04:09 +0000

    feat: refactor template

--- aoc_2025/aoc_template.py
+++ aoc_2025/aoc_template.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import date
 import sys
 sys.path.append('..')
@@ -7,15 +8,26 @@ day_num = date.today().day
 data = get_input(day_num)
 
 
-def part_1():
+def part_1(puzzle_input):
     return None
 
 
-def part_2():
+def part_2(puzzle_input):
     return None
 
 
 if __name__ == '__main__':
-    a1 = part_1()
-    a2 = part_2()
-    submit_answer(answer_1=a1, answer_2=a2, day_num=day_num)
+    parser = argparse.ArgumentParser(description='Advent of Code solution')
+    parser.add_argument('-t', '--test', action='store_true', help='Use test input')
+    parser.add_argument('-d', '--day_num', type=int, help='Day number for Advent of Code')
+    parser.add_argument('-p', '--sep', type=str, default='\n', help='Input separator')
+    parser.add_argument('-s', '--submit', action='store_true', help='Submit the answer to Advent of Code')
+    args = parser.parse_args()
+    if not args.day_num:
+        args.day_num = date.today().day
+
+    puzzle_input = get_input(day_num=args.day_num, test=args.test).split(args.sep)
+
+    a1 = part_1(puzzle_input)
+    a2 = part_2(puzzle_input)
+    submit_answer(answer_1=a1, answer_2=a2, day_num=args.day_num, submit=args.submit)
