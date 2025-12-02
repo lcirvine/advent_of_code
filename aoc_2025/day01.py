9ae538c8fc69e18b1caa3ebe3e54a933f57bdf30
Lance Irvine <lance.irvine@factset.com>
2025-12-02 19:04:54 +0000

    refactor day1

--- aoc_2025/day01.py
+++ aoc_2025/day01.py
@@ -1,15 +1,10 @@
+import argparse
+from datetime import date
 import sys
 sys.path.append('..')
 import re
 from aoc_utils import get_input, submit_answer
 
-day_num = 1
-data = get_input(day_num).split('\n')
-
-turn_mapping = {
-    'L': -1,
-    'R': 1
-}
 
 def get_direction(turn: str):
     return re.search(r'[A-Z]', turn).group()
@@ -20,14 +15,18 @@ def get_clicks(turn: str):
 def new_position(turn: str, current_pos: int):
     direction = get_direction(turn)
     clicks = get_clicks(turn)
+    turn_mapping = {
+        'L': -1,
+        'R': 1
+    }
     return current_pos + (clicks * turn_mapping[direction])
 
 
-def part_1():
+def part_1(puzzle_input):
     pos = 50
     num_zeros = 0
 
-    for turn in data:
+    for turn in puzzle_input:
         pos = new_position(turn, pos)
         pos %= 100
         if pos == 0:
@@ -36,12 +35,12 @@ def part_1():
     return num_zeros
 
 
-def part_2():
+def part_2(puzzle_input):
     pos = 50
     num_zeros = 0
     zero_clicks = 0
 
-    for turn in data:
+    for turn in puzzle_input:
         direction = get_direction(turn)
         clicks = get_clicks(turn)
         # if turning left, reverse position
@@ -61,6 +60,17 @@ def part_2():
 
 
 if __name__ == '__main__':
-    a1 = part_1()
-    a2 = part_2()
-    submit_answer(answer_1=a1, answer_2=a2, day_num=day_num, submit=True)
+    parser = argparse.ArgumentParser(description='Advent of Code solution')
+    parser.add_argument('-t', '--test', action='store_true', help='Use test input')
+    parser.add_argument('-d', '--day_num', type=int, help='Day number for Advent of Code')
+    parser.add_argument('-p', '--sep', type=str, default='\n', help='Input separator')
+    parser.add_argument('-s', '--submit', action='store_true', help='Submit the answer to Advent of Code')
+    args = parser.parse_args()
+    if not args.day_num:
+        args.day_num = date.today().day
+
+    puzzle_input = get_input(args.day_num, test=args.test).split(args.sep)
+
+    a1 = part_1(puzzle_input)
+    a2 = part_2(puzzle_input)
+    submit_answer(answer_1=a1, answer_2=a2, day_num=args.day_num, submit=args.submit)
